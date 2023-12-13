85a0dc3f1cf33493c5aa03477aecabe21eeeb78f
cleogldman <143565750+cleogldman@users.noreply.github.com>
2023-12-13 01:48:34 +0000

    trying to figure out environment

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 
 module.exports = function(eleventyConfig) {
+  console.log(process.env.NODE_ENV);
   let p = {}
   if (process.env.NODE_ENV === 'production') {
     console.log('running in production');
