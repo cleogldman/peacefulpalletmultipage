a624598c72b476fa604c14bf27d9125a13777b04
cleogldman <143565750+cleogldman@users.noreply.github.com>
2023-12-13 01:37:57 +0000

    adds 11ty URL transform code

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
-module.exports = function(eleventyConfig) {
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
+
+module.exports = function(eleventyConfig) {
+  let p = {}
+  if (process.env.NODE_ENV === 'production') {
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    p={ pathPrefix: "/runway-radar/"}
+  }
+  
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addWatchTarget("./src/");
   
     
     // Return your Object options:
     return {
+     ...p,
       dir: {
         input: "src",
         output: "docs",
